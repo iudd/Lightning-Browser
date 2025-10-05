4b284ade9d0a0207b01022c82a5a1d26a184073f
iudd <89436073@qq.com>
2025-10-05 10:26:26 +0800

    Initial commit: Lightning Browser with AGP 8.6.0 compatibility fix

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.6.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.20")
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.53.0")
+    }
+
+    extra.apply {
+        set("minSdkVersion", 21)
+        set("targetSdkVersion", 30)
+        set("buildToolsVersion", 30)
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
